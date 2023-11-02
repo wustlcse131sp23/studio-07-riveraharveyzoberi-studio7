d6e013e4661c27f62ce0d040ed6af149f7e19cb7
marti <marti@DESKTOP-DC4DD99>
2023-11-01 19:13:17 -0500

    w

--- src/studio7/Complex.java
+++ src/studio7/Complex.java
@@ -1,5 +1,44 @@
 package studio7;
 
 public class Complex {
-
+	private double real;
+	private double imaginary;
+	
+	public Complex(double real, double imaginary) {
+		this.real = real;
+		this.imaginary = imaginary;
+	}
+	
+	public Complex add(Complex other) {
+		double newReal = this.real + other.real;
+		double newImaginary = this.imaginary + other.imaginary;
+		return new Complex(newReal, newImaginary);
+	}
+	
+	public Complex multiply(Complex other) {
+		double newReal = (this.real * other.real) - (this.imaginary * other.imaginary);
+		double newImaginary = (this.real * other.imaginary) + (this.imaginary * other.real);
+		return new Complex(newReal, newImaginary);
+	}
+	
+	public String toString() {
+		if (imaginary >= 0) {
+			return real + " + " + imaginary + "i";
+		} else {
+			return real + " - " + (-imaginary) + "i";
+		}
+	}
+	
+	public static void main(String[] args) {
+		Complex complex1 = new Complex(3.0, 4.0);
+		Complex complex2 = new Complex(1.0, -2.0);
+		
+		Complex sum = complex1.add(complex2);
+		Complex product = complex1.multiply(complex2);
+		
+		System.out.println("Complex1: " + complex1);
+		System.out.println("Complex2: " + complex2);
+		System.out.println("Sum: " + sum);
+		System.out.println("Product: " + product);
+	}
 }
--- src/studio7/HockeyPlayer.java
+++ src/studio7/HockeyPlayer.java
@@ -1,5 +1,65 @@
 package studio7;
 
 public class HockeyPlayer {
+	private String name;
+	private int jerseyNumber;
+	private boolean rightHanded;
+    private boolean canShootEitherWay;
+    private int goals;
+    private int assists;
+    private int gamesPlayed;
 
+    public HockeyPlayer(String name, int jerseyNumber, boolean rightHanded, boolean canShootEitherWay) {
+        this.name = name;
+        this.jerseyNumber = jerseyNumber;
+        this.rightHanded = rightHanded;
+        this.canShootEitherWay = canShootEitherWay;
+        this.goals = 0;
+        this.assists = 0;
+        this.gamesPlayed = 0;
+    }
+
+    public void completeGame(int goals, int assists) {
+        this.goals += goals;
+        this.assists += assists;
+        this.gamesPlayed++;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public int getPoints() {
+        return goals + assists;
+    }
+
+    public int getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    @Override
+    public String toString() {
+        String handedness = rightHanded ? "Right-Handed" : "Left-Handed";
+        if (canShootEitherWay) {
+            handedness += " (Can Shoot Either Way)";
+        }
+        return "Name: " + name + ", Jersey Number: " + jerseyNumber + ", Handedness: " + handedness +
+               ", Goals: " + goals + ", Assists: " + assists + ", Points: " + getPoints() +
+               ", Games Played: " + gamesPlayed;
+    }
+
+    public static void main(String[] args) {
+        HockeyPlayer player1 = new HockeyPlayer("John Doe", 10, true, false);
+        HockeyPlayer player2 = new HockeyPlayer("Jane Smith", 20, false, true);
+
+        player1.completeGame(2, 1);
+        player2.completeGame(1, 3);
+
+        System.out.println(player1);
+        System.out.println(player2);
+    }
 }
